6062abdd52a4b105383f26cdcae7af0ff05ec44f
Pedro Sousa <pedro.marquessousa@student.manchester.ac.uk>
2024-01-01 19:23:38 +0000

    .

--- energy_funcs/cnn.py
+++ energy_funcs/cnn.py
@@ -87,3 +87,33 @@ class SimpleCNNModel_2(nn.Module):
     def forward(self, x):
         x = self.cnn_layers(x).squeeze(dim=-1)
         return x
+    
+
+class SimpleCNNModel_3(nn.Module):
+
+    def __init__(self, hidden_features=24, out_dim=1, **kwargs):
+        super().__init__()
+        # We increase the hidden dimension over layers. Here pre-calculated for simplicity.
+        c_hid1 = hidden_features//2
+        c_hid2 = hidden_features
+        c_hid3 = hidden_features*2
+
+        # Series of convolutions and Swish activation functions
+        self.cnn_layers = nn.Sequential(
+                nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4), # [18x18]
+                Swish(),
+                nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1), #  [9x9]
+                Swish(),
+                nn.Conv2d(c_hid2, c_hid3, kernel_size=3, stride=2, padding=1), # [5x5]
+                Swish(),
+                nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1), # [3x3]
+                Swish(),
+                nn.Flatten(),
+                nn.Linear(c_hid3*4, c_hid3),
+                Swish(),
+                nn.Linear(c_hid3, out_dim)
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
